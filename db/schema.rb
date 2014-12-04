bf836b32b47fc6e9950851886e67d54ee71e5ad9
Myla Baker <myla.baker@gmail.com>
2014-12-03 16:17:27 -0800

    PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141203231658) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "product_purchases", force: true do |t|
     t.integer  "product_id"
     t.integer  "purchase_id"
